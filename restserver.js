0020ad40af343e9a108c06d182bb6198d946e232
gitgitgitbert <wilczek.adalbert@web.de>
2017-03-14 15:57:34 +0100

    First Post

--- restserver.js
+++ restserver.js
@@ -1,13 +1,30 @@
 var express = require("express");
 var _ = require("lodash");
+var bodyParser = require('body-parser')
+
+/** TODO: 
+ *  - Use Bodyparser?
+ */
 
 module.exports = (function(){
     var app = express();
+
+    //TODO: Exact Task of this
+    //app.configure(function(){app.use(app.router)});
+    
+    // create application/json parser 
+    var jsonParser = bodyParser.json()
+    
+    // create application/x-www-form-urlencoded parser 
+    var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
     var cachedData;
 
     /* FOLDERS */
     /** List */
     app.get('/folders', function (req, res) {
+        console.log("Folders requested");
         if(cachedData && cachedData.folders){
             res.end(JSON.stringify(cachedData.folders));    
         }
@@ -18,6 +35,7 @@ module.exports = (function(){
 
     /** Get */
     app.get('/folders/:id', function(req, res){
+        console.log("Specific Folder requested");
         if(cachedData && cachedData.folders){
             var requested = _.find(cachedData.folders,"id",req.params.id);
             if(requested)
@@ -34,7 +52,7 @@ module.exports = (function(){
     })
 
     /** Add */
-    app.post('/folders', function (req, res) {
+    app.post('/folders',jsonParser,  function (req, res) {
         // First read existing users.
         // fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
         //     data = JSON.parse( data );
@@ -42,7 +60,40 @@ module.exports = (function(){
         //     console.log( data );
         //     res.end( JSON.stringify(data));
         // });
-        console.log("Added: ");
+        // TODO NEXT: Body is empty, which is said to because
+        // of wrong configuration?
+        try{
+            console.log("Method: " +req.method);
+            console.log("Url: " + req.url);
+            console.log("Headers: " + JSON.stringify(req.headers));
+            console.log("User Agent: " + req.headers['user-agent']);
+
+            if(req.body && req.body.awdata){
+                console.log("Added: " + req.body.awdata);
+            }
+
+            // //This is going to be simplyfied by e.g. body or Bodyparser
+            // var body = [];
+            // req.on('data', function(chunk) {
+            //     body.push(chunk);
+            // }).on('end', function() {
+            //     body = Buffer.concat(body).toString();
+            //     // at this point, `body` has the entire request body stored in it as a string
+            //     console.log("Body " + body);
+
+            // });
+
+            if (!req.body) return res.sendStatus(400);
+            
+            console.log("Body: " + JSON.stringify(req.body));
+            // create user in req.body 
+        } catch(e)
+        {
+            console.log("Error on post");
+        }
+
+        //The newly added object should be returned
+        return res.sendStatus(200);
     })
 
 
